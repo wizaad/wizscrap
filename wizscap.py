0e4ed29aa7ceb89a1d66cde5f011edfc9a6227dd
Wizaad <jongeek47@gmail.com>
2021-06-08 00:57:53 +0300

    custom input for location and type of biz

--- wizscap.py
+++ wizscap.py
@@ -3,21 +3,33 @@ import config
 
 import argparse
 
+print('''
+ __      __.__                           .___
+/  \    /  \__|____________  _____     __| _/
+\   \/\/   /  \___   /\__  \ \__  \   / __ | 
+ \        /|  |/    /  / __ \_/ __ \_/ /_/ | 
+  \__/\  / |__/_____ \(____  (____  /\____ | 
+       \/           \/     \/     \/      \/ 
+        https://github.com/Wizaad/wizscrap
+           https://twitter.com/MrWizaad
+''')
+
 parser = argparse.ArgumentParser(description='A simple program to help search businesses with high rating.')
 parser.print_help()
 
 url = "https://api.yelp.com/v3/businesses/search"
+
 headers = {
     "Authorization": "Bearer " + config.api_key
 }
 
 params = {
-    "term": "AirBnB",
-    "location": "Nairobi"
+    "term": input("What business are you looking for?  "),
+    "location": input("What city do you want? ")
 }
 
 response = requests.get(url, headers=headers, params=params)
 businesses = response.json()["businesses"]
 names = [business["name"]
-        for business in businesses if business["rating"] > 4.5]
+         for business in businesses if business["rating"] > 4.5]
 print(names)
